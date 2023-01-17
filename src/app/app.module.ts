59b711a308d23fab3d6fdb7d7868056cdb868b4a
rajesh <kemar.dev1@outlook.com>
2023-01-17 10:12:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule , HttpClient } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+
+function myFunc(httpClient: HttpClient)
+{
+	return () => {  //anonymous function start
+			return Promise.resolve(true)
+				.then( _ => {alert("1");})
+				.then( _ => {alert("2");})
+				.then( _ => {alert("3");})
+				.then( _ => {alert("4");})
+				;
+		     } 	//anonymous function end
+}
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+	{
+	provide : APP_INITIALIZER, 
+	useFactory : myFunc, 
+	multi : true, 
+	deps : [HttpClient]
+	}
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
